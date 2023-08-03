1ff2ff46429c75c0d65a8c60a8d8561e4c7cf8b6
ProgramEze <141289604+ProgramEze@users.noreply.github.com>
2023-08-02 23:10:21 -0300

    mas cambios

--- src/proyectoparasubiragh/ProyectoParaSubirAGH.java
+++ src/proyectoparasubiragh/ProyectoParaSubirAGH.java
@@ -16,6 +16,11 @@ public class ProyectoParaSubirAGH {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Soy un comentario
+        /*soy un
+        comentario mas
+        largo
+        */
     }
     
 }
---
+++ src/proyectoparasubiragh/otroArchivo.java
@@ -0,0 +1,4 @@
+package proyectoparasubiragh;
+public class otroArchivo {
+
+}
